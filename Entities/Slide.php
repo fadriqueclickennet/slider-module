d86b7656298fa53dc3e16d9980a1f8f322bf9144
fadriqueclickennet <48823970+fadriqueclickennet@users.noreply.github.com>
2020-03-25 12:31:35 +0100

    Update Slide.php

--- Entities/Slide.php
+++ Entities/Slide.php
@@ -2,7 +2,7 @@
 
 namespace Modules\Slider\Entities;
 
-use Dimsav\Translatable\Translatable;
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
